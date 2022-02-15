9dc43475abbd4ec29aa41ae1173b0aad37effee2
StanaAndrei <stadey33@gmail.com>
2022-02-15 21:42:48 +0200

    solved mzviewcontroller bug

--- eye-maze-v2.0/src/main/java/eyemazev20/controllers/views/MazeViewController.java
+++ eye-maze-v2.0/src/main/java/eyemazev20/controllers/views/MazeViewController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.HttpSession;
 
+@SessionAttributes("loginUUID")
 @Controller
 public class MazeViewController {
     @GetMapping(value = {"/mkmaze", "/mkmaze/{mzname}"})
